74b023571a83b70eee93621dd9a53a27949d34ff
Yongjung Shin <syj4209@naver.com>
2021-04-29 16:18:23 +0900

    first commit

---
+++ Assets/02.Scripts/CameraResolution.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResolution : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1000,800, false);
+    }
+}
